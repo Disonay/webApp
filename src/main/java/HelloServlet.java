6fceff9c86fb0dae91099c1e533f75cbee240a59
Sergei <kolesniksn2510@gmail.com>
2022-07-11 03:15:16 +0300

    Add servlet with hello

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,20 @@
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Logger;
+
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        try (PrintWriter pw = resp.getWriter()) {
+            pw.write("<h1>Hello, Сергей<h1>");
+        }
+    }
+}
